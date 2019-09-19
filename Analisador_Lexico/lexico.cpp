86f0b3b4f848593a3b0477557e053fb1e010ef02
Krysthian Lessa <krysthianlessa@gmail.com>
2019-09-19 16:52:58 -0300

    Implementa leitura do buffer e cria a função analisadorLexico()

--- Analisador_Lexico/lexico.cpp
+++ Analisador_Lexico/lexico.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+#define Q0 0
+#define ID 1
+#define NUM 2
+
+typedef struct {
+    string str;
+} Lexema;
+
+typedef struct {
+    int estado;
+    int ult_linha;
+    int ult_coluna;
+    string str_acumulado;
+} MaquinaDeEstados;
+
+bool analisadorLexico(string buffer){
+    return true;
+}    
+
+
 int main()
 {
+    char c;
+    string buffer;
+
+    while((c = getchar()) && (c != EOF))
+        buffer += c;
+    buffer += -1;
+
+    if (analisadorLexico(buffer))
+        cout << "Analise léxica concluída com sucesso!" << endl;
+    
     
     return 0;
 }
