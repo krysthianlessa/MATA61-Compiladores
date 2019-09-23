d89450d8203a4a067b947f439c9f16e2ce5accfa
Krysthian Lessa <krysthianlessa@gmail.com>
2019-09-23 15:53:28 -0300

    Implementa as funções de reconhecimentos de letra e numero e começa a função analisadorlexico()

--- Analisador_Lexico/lexico.cpp
+++ Analisador_Lexico/lexico.cpp
@@ -19,23 +19,97 @@ typedef struct {
     string str_acumulado;
 } MaquinaDeEstados;
 
-bool analisadorLexico(string buffer){
-    return true;
+MaquinaDeEstados maquina;
+int linha = 1;
+int coluna = 1;
+
+bool ehCharValido(char c) {
+    return ( c == 32 || c == 9 || c == 10 || c == -1 ||
+            (c >= 48 && c <= 57) || 
+            (c >= 65 && c <= 90) || 
+            (c >= 97 && c <= 122));
+}
+
+bool eh_letra(char c) {
+    return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
+}
+
+bool eh_numero(char c) {
+    return (c >= 48 && c <= 57);
+}
+
+bool eh_separador(char c) {
+    return ((c == 9) || (c == 10) || (c == 32));
+}
+
+bool eh_EOF(char c){
+    return (c == -1);
+}
+
+void consome(char c){
+    maquina.str_acumulado += c;
+    maquina.ult_linha = linha;
+    maquina.ult_coluna = coluna;
+}
+
+int qzero(char c) {
+    if (eh_letra(c)) {
+        consome(c);
+        return ID;
+    } else if (eh_numero(c)) {
+        consome(c);
+        return NUM;
+    } else if (eh_separador(c)) {
+        
+    } else if (eh_EOF(c)) {
+        
+    }
+    
+    
+}
+
+int id(char c) {
+
+}
+
+int num(char c){
+
+}
+
+void analisadorLexico(string buffer) {
+    maquina.estado = Q0;
+    
+    for(int i = 0; i < buffer.size(); i++, coluna++){
+        switch (maquina.estado) {
+            case Q0:
+                maquina.estado = qzero(buffer[i]);
+                break;
+            case ID:
+                maquina.estado = id(buffer[i]);
+                break;
+            case NUM:
+                maquina.estado = num(buffer[i]);
+                break;
+            default:
+                cout << "Tratar alguma coisa!\n";
+                break;
+        }
+    }
 }    
 
 
 int main()
 {
-    char c;
     string buffer;
+    char c;
 
     while((c = getchar()) && (c != EOF))
         buffer += c;
     buffer += -1;
 
-    if (analisadorLexico(buffer))
-        cout << "Analise léxica concluída com sucesso!" << endl;
+    analisadorLexico(buffer);
     
+    cout << "Analise léxica concluída com sucesso!" << endl;
     
     return 0;
 }
